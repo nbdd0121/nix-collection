11357230385ef75c7e22ddafff0c01794160113e
Gary Guo <gary@garyguo.net>
2026-08-08 01:34:08 +0100

    coccinelle4rust: update

--- pkgs/coccinelle4rust.nix
+++ pkgs/coccinelle4rust.nix
@@ -6,16 +6,16 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "coccinelle4rust";
-  version = "0.1.0-unstable-20251205";
+  version = "0.1.0-unstable-20260615";
 
   src = fetchgit {
     url = "https://gitlab.inria.fr/coccinelle/coccinelleforrust.git";
     fetchSubmodules = false;
-    rev = "6a9989cec0d18952b08a0b3db0ba72abe9727993";
-    sha256 = "sha256-vHCAL4Ls3DHuOW8pju0SnnaSvMcQHDZSeXCFyQOHLoM=";
+    rev = "86de52a92354f2af5d2e3d204ccfbf97258c2b3b";
+    sha256 = "sha256-LN421TtYvy2D5SClYUDM1DZNehBT9xeSYDEPFPDm34o=";
   };
 
-  cargoHash = "sha256-8OkFn2uX3rNsZLoLc2PXyVYO/POzfQsn99YIwDMyUQo=";
+  cargoHash = "sha256-fKbv1oau0UBGD1/n9TWCARhGKhvd3s2wliIS4fxZO6E=";
 
   nativeBuildInputs = [ python3 ];
 
